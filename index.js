a87c343f6294c7283a0fa6e007aeeae6cb1cdbe4
Rafijoee <rafijoe44@gmail.com>
2024-10-26 10:24:06 +0700

    init setup

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+const flash = require('express-flash');
+const router = require('./routes/auth.routes'); // pastikan router sudah diimport atau sesuaikan dengan path router Anda
+
+const app = express();
+const port = process.env.PORT || 3000; // Menggunakan port dari .env jika ada
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'secret', // Gunakan .env untuk menyimpan nilai rahasia
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(morgan('dev'));
+app.use(flash());
+
+const authRouter = require('./routes/auth.routes');
+app .use('/', authRouter);
+
+
+app.listen(port, () => {
+    console.log(`I LOVE YOU ${port} ===> biar sama kayak mas febrian wkwkwk`);
+});
